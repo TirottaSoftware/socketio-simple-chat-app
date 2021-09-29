6f7fb97630bdc120147509fb75c9c42bfac85315
Cristian <i449915@student.fontys.nl>
2021-09-29 21:35:17 +0300

    Sockets basic setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
+import io from 'socket.io-client';
 
 function App() {
+  const socket = io.connect('http://localhost:3001');
+  
   return (
     <div className="App">
       <h1>Hello React!</h1>
--- server/server.js
+++ server/server.js
@@ -1,1 +1,31 @@
- 
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin: "http://localhost:3000"
+    }
+});
+
+io.on('connection', () => {
+    console.log('Connection established.')
+    io.on('disconnect', () => {
+        console.log('Disconnected.')
+    })
+})
+
+io.on('connect', socket => {
+    console.log(socket.id)
+})
+
+server.listen(3001, () => {
+    console.log('Listening on 3000')
+})
