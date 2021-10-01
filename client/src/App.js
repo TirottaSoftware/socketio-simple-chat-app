d31a9faa426648f56ad79e05dbb5dc190900b465
Cristian <i449915@student.fontys.nl>
2021-10-01 18:14:02 +0300

    Added styling and more room info

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,12 @@ function App() {
   
   const [username, setUsername] = useState('');
   const [room, setRoom] = useState('');
+  const [connected, setConnected] = useState(false);
 
   const joinRoom = (e) => {
     e.preventDefault();
     socket.emit('join-room', room, username)
+    setConnected(true);
   }
 
   const handleUsernameInputChange = (e) => {
@@ -26,10 +28,16 @@ function App() {
 
   return (
     <div className="App">
-      <input type = 'text' onChange = {handleUsernameInputChange} value = {username} placeholder = 'Username...' />
-      <input type = 'text' onChange = {handleRoomInputChange} value = {room} placeholder = 'Room...' />
-      <button onClick = {joinRoom}>Join Room</button>
-      <Chat room = {room} username = {username} socket = {socket} />
+      {
+        !connected?<div className = 'room-form'>
+          <h1>Join a Room</h1>
+          <input type = 'text' onChange = {handleUsernameInputChange} value = {username} placeholder = 'Username...' />
+          <input type = 'text' onChange = {handleRoomInputChange} value = {room} placeholder = 'Room...' />
+          <button onClick = {joinRoom}>Join Room</button>
+        </div>
+        :
+        <Chat room = {room} username = {username} socket = {socket} />
+      }
     </div>
   );
 }
--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 const Chat = ({socket, username, room}) => {
     const [message, setMessage] = useState('');
@@ -34,42 +35,47 @@ const Chat = ({socket, username, room}) => {
     }, [socket])
 
     return (
-        <div className = 'chat-box'>
-            <div className = 'chat-body'>
-                {messagesList.map(msg => {
-                    let msgClass = '';
-                    if(msg.username === username){
-                        msgClass = 'msg msg-green';
-                    }
-                    else{
-                        msgClass = 'msg msg-blue';
-                    }
+        <> 
+            <h1 className = 'chat-room-nr'>Room Number: {room}</h1>
+            <div className = 'chat-box'>
+                <ScrollToBottom className = 'msg-scroll'>
+                <div className = 'chat-body'>
+                    {messagesList.map(msg => {
+                        let msgClass = '';
+                        if(msg.username === username){
+                            msgClass = 'msg msg-green';
+                        }
+                        else{
+                            msgClass = 'msg msg-blue';
+                        }
 
-                    if(msg.type === 'msg-info'){
-                        return <div key = {msg.username + ' ' + Math.random() * 10} className = 'msg msg-info'>{msg.username} joined {msg.room}</div>
-                    }
-                    else{
-                        return <div key = {msg.message + ' ' + Math.random() * 10} className = {msgClass}>
-                            <div className = 'msg-body'>
-                                <p>{msg.username} said: {msg.message}</p>
-                                <p className = 'msg-timestamp'>{msg.time}</p>
+                        if(msg.type === 'msg-info'){
+                            return <div key = {msg.username + ' ' + Math.random() * 10} className = 'msg msg-info'>{msg.username} joined {msg.room}</div>
+                        }
+                        else{
+                            return <div key = {msg.message + ' ' + Math.random() * 10} className = {msgClass}>
+                                <div className = 'msg-body'>
+                                    <p>{msg.username} said: {msg.message}</p>
+                                    <p className = 'msg-timestamp'>{msg.time}</p>
+                                </div>
                             </div>
-                        </div>
-                    }
-                })}
+                        }
+                    })}
+                </div>
+                </ScrollToBottom>
+                <div className = 'chat-msg-box'>
+                    <input type = 'text' value = {message} 
+                    onChange = {(e) => {setMessage(e.target.value)}}
+                    onKeyPress = {(e) => {
+                        if(e.key === 'Enter'){
+                            sendMessage();
+                        }
+                    }} 
+                    />
+                    <button onClick = {sendMessage}>Send</button>
+                </div>
             </div>
-            <div className = 'chat-msg-box'>
-                <input type = 'text' value = {message} 
-                onChange = {(e) => {setMessage(e.target.value)}}
-                onKeyPress = {(e) => {
-                    if(e.key === 'Enter'){
-                        sendMessage();
-                    }
-                }} 
-                />
-                <button onClick = {sendMessage}>Send</button>
-            </div>
-        </div>
+        </>
     )
 }
 
